fc77a4251bf24b4b2a7a1cf3dfc4c59f97790b87
riqi3 <enrique.pacudan@usjr.edu.ph>
2022-12-07 16:20:43 +0800

    functional camera

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         // primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(cameras: [],),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:camera/camera.dart';
+import 'package:camera_platform_interface/src/types/camera_description.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -7,27 +9,104 @@ import '../screens/home/myjams.dart';
 import '../screens/home/sidenav.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+    const HomeScreen({Key? key, required this.cameras}) : super(key: key);
+
+  final List<CameraDescription>? cameras;
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late CameraController _cameraController;
+  bool _isRearCameraSelected = true;
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera(widget.cameras![0]);
+  }
+
+
+  Future initCamera(CameraDescription cameraDescription) async {
+    _cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+    try {
+      await _cameraController.initialize().then((_) {
+        if (!mounted) return;
+        setState(() {});
+      });
+    } on CameraException catch (e) {
+      debugPrint("camera error $e");
+    }
+  }
+
+
+
   int currentTab = 0;
 
   List<Widget> screens = <Widget>[
-    HomeScreen(),
+    HomeScreen(cameras: [],),
     AlarmScreen(),
     MyJamsScreen(),
     SideNavMenu(),
   ];
 
-  Widget currentScreen = HomeScreen();
+  Widget currentScreen = HomeScreen(cameras: [],);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+      child: Stack(children: [
+        
+        (_cameraController.value.isInitialized)
+            ? CameraPreview(_cameraController)
+            : Container(
+                color: Colors.black,
+                child: const Center(child: CircularProgressIndicator())),
+        // Align(
+        //     alignment: Alignment.bottomCenter,
+        //     child: Container(
+        //       height: MediaQuery.of(context).size.height * 0.20,
+        //       decoration: const BoxDecoration(
+        //           borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+        //           color: Colors.black),
+        //       child:
+        //           Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        //         Expanded(
+        //             child: IconButton(
+        //           padding: EdgeInsets.zero,
+        //           iconSize: 30,
+        //           icon: Icon(
+        //               _isRearCameraSelected
+        //                   ? CupertinoIcons.switch_camera
+        //                   : CupertinoIcons.switch_camera_solid,
+        //               color: Colors.white),
+        //           onPressed: () {
+        //             setState(
+        //                 () => _isRearCameraSelected = !_isRearCameraSelected);
+        //             initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
+        //           },
+        //         )),
+        //         Expanded(
+        //             child: IconButton(
+        //           onPressed: takePicture,
+        //           iconSize: 50,
+        //           padding: EdgeInsets.zero,
+        //           constraints: const BoxConstraints(),
+        //           icon: const Icon(Icons.circle, color: Colors.white),
+        //         )),
+        //         const Spacer(),
+        //       ]),
+        //     )),
+      ]),
+    ),
       floatingActionButton: Container(
         height: 60,
         width: 60,
