4e5623cad03c838b8f4b0cecc2dd0a6c76ae01d5
riqi3 <enrique.pacudan@usjr.edu.ph>
2022-12-11 21:30:53 +0800

    added alarm

--- android/app/src/main/kotlin/com/example/wmaproject/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wmaproject/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.wmaproject
 
-import io.flutter.embedding.android.FlutterActivity
+import  io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
